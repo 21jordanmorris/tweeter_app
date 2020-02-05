4ac7dbe478de1d5e17725ff42c09278140a565ae
Jordan Morris <21jordanmorris@gmail.com>
2020-02-05 12:09:56 -0600

    configuring for heroku

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'x6$%u*scrxfv37-*^$poggmo&w8a56^jt5@n+@r_$0lxo8%hoh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
