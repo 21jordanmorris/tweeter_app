8f95a675302691fad64ed0c7b4497638bcf8ad3c
Jordan Morris <21jordanmorris@gmail.com>
2020-02-20 14:27:41 -0600

    Add email feature

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -113,6 +113,13 @@ BOOTSTRAP4 = { 'include_jquery' : True }
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.getenv('SENDGRID_USER') # new
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD') # new
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
